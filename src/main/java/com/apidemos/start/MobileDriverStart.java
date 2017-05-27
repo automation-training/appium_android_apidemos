020feef8b00de4949a9fc9de263baed57129d548
Roma Romanik <romanikroma@gmail.com>
2017-05-27 23:55:59 +0300

    init commit

---
+++ src/main/java/com/apidemos/start/MobileDriverStart.java
@@ -0,0 +1,46 @@
+package com.apidemos.start;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class MobileDriverStart {
+
+	@Test
+	public void testDialogTitle() throws MalformedURLException {
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("platformName", "android");
+		capabilities.setCapability("platformVersion", "7.1.1");
+		capabilities.setCapability("deviceName", "Emulator");
+		capabilities.setCapability("avd", "Pixel_XL_API_25");
+		capabilities.setCapability("app", "E:\\android\\apps\\ApiDemos-debug.apk");
+
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		AndroidDriver<MobileElement> driver = new AndroidDriver<>(url, capabilities);
+
+		MobileElement appTab = driver.findElementByAccessibilityId("App");
+		appTab.click();
+
+		MobileElement alertDialogsTab = driver.findElementByAccessibilityId("Alert Dialogs");
+		alertDialogsTab.click();
+
+		driver.findElementByAccessibilityId("Text Entry dialog").click();
+
+		MobileElement nameInput = driver.findElement(By.id("io.appium.android.apis:id/username_edit"));
+		nameInput.sendKeys("Appium");
+
+		MobileElement alertTitleElement = driver.findElement(By.id("android:id/alertTitle"));
+		String actualTitle = alertTitleElement.getText();
+		String expectedTitle = "Text Entry Dialog";
+		
+		Assert.assertEquals(actualTitle, expectedTitle);
+	}
+}
